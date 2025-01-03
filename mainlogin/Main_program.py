13ddcbd3ef397354848e5cedd8464f1c56adbe15
Pankaj Saratkar <108680739+PANKAJ11111111@users.noreply.github.com>
2025-01-03 20:42:26 +0530

    registration complete pankaj

--- mainlogin/Main_program.py
+++ mainlogin/Main_program.py
@@ -1,7 +1,11 @@
 import os
 import user_class as p
+import pickle
+import datetime
 
 # here main login of code prasent
+
+
 
 def register():
     os.system('cls') 
@@ -19,9 +23,17 @@ def register():
     
     # Ensure the directory exists
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    
+    file = open(file_path,'wb')
 
-    
-    
+    pickle.dump(user1,file)
+    file.close()
+    print('Registration successful!')
+    print('Now Login..')
+
+    os.system('cls')
+    print("Login Now...")
+    intro()
 
 
 def login():
@@ -33,31 +45,40 @@ def exit():
     print("Thank you for using Personal Expense Tracker. Goodbye!".center(100," "))
     print()
 
-print()
-# welcome greet
-print(" Hello Welocome To Personal Expenses Managment System ".center(100,"="))
-print()
 
-# option
-
-print('1. Register ')
-print('2. Login ')
-print('3. Exit')
-
-#userchoise variable store user choise
-userchoise = int(input('Enter Your Choise (1/2/3): '))
-
-if(userchoise == 1):
-    register()
-
-elif(userchoise == 2):
-    login()
-
-elif(userchoise == 3):
-    exit()
-
-else:
+def intro():
     print()
-    print("Soory! You Enter Invalid Option.")
+    # welcome greet
+    print(" Hello Welocome To Personal Expenses Managment System ".center(100,"="))
     print()
 
+     # option
+
+    print('1. Register ')
+    print('2. Login ')
+    print('3. Exit')
+
+    #userchoise variable store user choise
+    userchoise = int(input('Enter Your Choise (1/2/3): '))
+
+    if(userchoise == 1):
+       register()
+
+    elif(userchoise == 2):
+      login()
+
+    elif(userchoise == 3):
+      exit()
+
+    else:
+     print()
+     print("Soory! You Enter Invalid Option.")
+     print()
+
+   
+
+intro()
+
+
+
+    
