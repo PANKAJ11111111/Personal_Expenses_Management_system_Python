58c74314a69101900e9ac6d2909a632dcd7bf1f7
Pankaj Saratkar <108680739+PANKAJ11111111@users.noreply.github.com>
2025-01-03 10:40:27 +0530

    main page create pankaj

--- mainlogin/Main_program.py
+++ mainlogin/Main_program.py
@@ -1,6 +1,40 @@
 # here main login of code prasent
 
+def register():
+    print("here we implement code for registration")
+
+def login():
+    print("code for Login ")
+
+def exit():
+    print()
+    print("Thank you for using Personal Expense Tracker. Goodbye!".center(100," "))
+    print()
+
 print()
 # welcome greet
 print(" Hello Welocome To Personal Expenses Managment System ".center(100,"="))
 print()
+
+# option
+
+print('1. Register ')
+print('2. Login ')
+print('3. Exit')
+
+#userchoise variable store user choise
+userchoise = int(input('Enter Your Choise (1/2/3): '))
+
+if(userchoise == 1):
+    register()
+
+elif(userchoise == 2):
+    login()
+
+elif(userchoise == 3):
+    exit()
+
+else:
+    print()
+    print("Soory! You Enter Invalid Option.")
+    print()
