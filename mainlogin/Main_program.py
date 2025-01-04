170521bc94fe68911010a39bb3b7b673e99de110
Pankaj Saratkar <108680739+PANKAJ11111111@users.noreply.github.com>
2025-01-04 18:38:13 +0530

    login also complete pankaj

--- mainlogin/Main_program.py
+++ mainlogin/Main_program.py
@@ -19,30 +19,70 @@ def register():
     base_dir = os.path.dirname(__file__)  # Directory of the current script
     
     file_path = os.path.join(base_dir, '..', 'userdatabase', 'userdata.txt')
-    print(file_path)
+
     
     # Ensure the directory exists
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     
-    file = open(file_path,'wb')
+    file = open(file_path,'ab')
 
     pickle.dump(user1,file)
     file.close()
+    
+    os.system('cls')
     print('Registration successful!')
     print('Now Login..')
 
-    os.system('cls')
-    print("Login Now...")
     intro()
 
 
 def login():
-    print("code for Login ")
+    os.system('cls') 
+    print()
+    print(" Hello Welocome To Personal Expenses Managment System Login ".center(100,"="))
+    print()
+    useremail = input("Enter Your Register Mail = ")
+    userpassword = input("Enter Your Password = ")
+    base_dir = os.path.dirname(__file__)  # Directory of the current script
+    
+    file_path = os.path.join(base_dir, '..', 'userdatabase', 'userdata.txt')
+
+    readfile = open(file_path, "rb")
+
+    try:
+    
+     for _ in range(100):
+        data1 = pickle.load(readfile)
+
+        if(useremail.lower() == data1.user_email.lower()):
+          if(userpassword == data1.user_password):
+             print()
+             print("Hello {}! It's always a pleasure to assist you. ❤️".format(data1.user_name)) 
+             print("Let’s get started on keeping your finances organized and stress-free")
+             print()
+             break
+          else:
+             os.system('cls')
+             print("You Enter Invalid Password! ❌ Try Again...")
+             intro()
+             
+        
+        
+    except EOFError:
+      os.system('cls')
+      print("User Not Found In DataBase Register First.... 🌍 ")
+      intro()
+
+    finally:
+      readfile.close()
+       
+       
+    
 
 
 def exit():
     print()
-    print("Thank you for using Personal Expense Tracker. Goodbye!".center(100," "))
+    print("Thank you for using Personal Expense Tracker. ✨ Goodbye! ✨".center(100," "))
     print()
 
 
@@ -72,7 +112,7 @@ def intro():
 
     else:
      print()
-     print("Soory! You Enter Invalid Option.")
+     print("Soory! You Enter Invalid Option 😵😵 .")
      print()
 
    
--- mainlogin/user_class.py
+++ mainlogin/user_class.py
@@ -2,13 +2,13 @@ class user:
     #attributes of user
     user_name = "None"
     user_email = "None"
-    __user_password = "none"
+    user_password = "none"
     
     def __init__(self):
         
         self.user_name = input("Enter Your Name = ")
         self.user_email =input("Enter Your Email =  ")
-        self.__user_password = input("Enter Your Password = ")
+        self.user_password = input("Enter Your Password = ")
         self.user_salary = int(input("Enter Your Salary = "))
         self.user_Target = int(input("Enter Your Monthly Expenses Target =  "))
         self.user_mobile = int(input("Enter Your Contect Number = "))
